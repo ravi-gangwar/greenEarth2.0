b4b851de5440584b1d0dd3f25932eb08ef1e8b7a
Ravi Gangwar <ravigangwar7465@gmail.com>
2024-12-04 21:05:12 +0530

    ADDED PLANTS

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.pinimg.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'upload.wikimedia.org',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // Match all API routes
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://greenearth2.vercel.app',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
