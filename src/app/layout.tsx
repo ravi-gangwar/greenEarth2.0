6250ea394e5d0e3f2ae49318d648b3fce9e95e58
Ravi Gangwar <ravigangwar7465@gmail.com>
2024-12-21 01:32:16 +0530

    ADD THEME_COLOR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <header>
-        <meta name="theme-color" content="#e9c46a" />
+        <meta name="theme-color" content="#F1EA69" />
       </header>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
